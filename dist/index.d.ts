de24b9c38b521c2f40c0540a421fa30b5dfb173f
DanWhitney <dwhitney197@gmail.com>
2022-01-07 13:41:46 +0000

    initial commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+export { viClose } from './ni-visa/vi_close';
+export { viOpen } from './ni-visa/vi_open';
+export { viOpenDefaultRM } from './ni-visa/vi_open_default_r_m';
+export { viRead } from './ni-visa/vi_read';
+export { viWrite } from './ni-visa/vi_write';
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Svisa',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
   },
   externals: [nodeExternals()],
 };
